eabace2eafada36d7b6beae0af301a58ce241f3c
richarda9 <rargomedoc89@gmail.com>
2017-04-16 16:18:16 -0500

    rad

--- gestionProyectos/src/main/java/com/sigcomt/gestionProyectos/repositorio/anteproyecto/ProyectoDao.java
+++ gestionProyectos/src/main/java/com/sigcomt/gestionProyectos/repositorio/anteproyecto/ProyectoDao.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import com.sigcomt.gestionProyectos.dominio.administracion.Proyecto;
+import com.sigcomt.gestionProyectos.model.anteproyecto.BuscarAnteproyectoModel;
 
 public interface ProyectoDao {
 	
 	public List<Proyecto> listarProyectoByDetalleEstadoProyectoByEstado(Map<String, Object> params);
+	public List<Proyecto> listarProyectoByEstado(int estado);
+	
 
+	public List<Proyecto> buscarProyectoByDetalleEstadoProyectoByEstado(BuscarAnteproyectoModel buscarAnteproyectoModel) ;
 }
--- gestionProyectos/src/main/java/com/sigcomt/gestionProyectos/servicio/anteproyecto/ProyectoService.java
+++ gestionProyectos/src/main/java/com/sigcomt/gestionProyectos/servicio/anteproyecto/ProyectoService.java
@@ -3,9 +3,13 @@ package com.sigcomt.gestionProyectos.servicio.anteproyecto;
 import java.util.List;
 
 import com.sigcomt.gestionProyectos.dominio.administracion.Proyecto;
+import com.sigcomt.gestionProyectos.model.anteproyecto.BuscarAnteproyectoModel;
 
 public interface ProyectoService {
 	
 	public List<Proyecto> listarProyectoByDetalleEstadoProyectoByEstado(int estado, Long idEstadoProyecto) throws Exception;
+	
+	public List<Proyecto> buscarProyectoByDetalleEstadoProyectoByEstado(BuscarAnteproyectoModel buscarAnteproyectoModel) throws Exception;
 
+	public List<Proyecto> listarProyectoByEstado(int estado);
 }
